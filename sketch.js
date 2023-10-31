fbe5fd3a55604d0639f58c7f9fdbe80c96e01063
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2023-10-31 22:46:17 +0000

    Ejercicio finalizado

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
+let foto
+
 function preload() {
   // put preload code here
+  foto = loadImage("./images/lisa.jpg")
 }
 
 function setup() {
   // put setup code here
-  createCanvas(800,800, WEBGL)
+  createCanvas(windowWidth,windowHeight, WEBGL)
 }
 
 function draw() {
   // put drawing code here
-  background(255)
+  background(232,167,152)
   rotateX(frameCount * 0.01)
   rotateY(frameCount * 0.01)
   rotateZ(frameCount * 0.01)
-  box(150)
+  noStroke()
+  texture(foto)
+  box(300)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
